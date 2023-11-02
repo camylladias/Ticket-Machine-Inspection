efd9be0a40d985e2f559f598218351043f8ff593
Lucca Guarreschi <luccaguareschi@gmail.com>
2023-11-02 00:27:58 -0300

    PapelMoedaTest Test

--- Source Code Inspection/srcTest/br/calebe/ticketmachine/core/PapelMoedaTest.java
+++ Source Code Inspection/srcTest/br/calebe/ticketmachine/core/PapelMoedaTest.java
@@ -1,6 +1,7 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 package br.calebe.ticketmachine.core;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 class PapelMoedaTest {
 
